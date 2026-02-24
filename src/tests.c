5d512c4763334137ab4babf584fc5d6630a17788
jayshozie <143549527+jayshozie@users.noreply.github.com>
2026-02-24 20:48:49 +0300

    chore: add Gemini as contributor to tests.c

--- src/tests.c
+++ src/tests.c
@@ -1,3 +1,21 @@
+/* allocs - Custom memory allocators written as a study exercise.
+Copyright (C) 2026  Emir Baha Yıldırım
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
+/* Assisted by: Gemini:gemini-3.1-pro-preview */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
